7b3d148d5f53c238cdd265b69f78f3296755dbe8
ItmanBuildsClouds <itmanpiotr.js@gmail.com>
2025-09-01 01:44:13 +0200

    fix: Assign correct IAM Role ARN to Lambda function

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -7,7 +7,7 @@ data archive_file "lambda" {
 resource "aws_lambda_function" "contact_form_lambda" {
     filename = data.archive_file.lambda.output_path
     function_name = "${var.project_name}-function"
-    role = aws_iam_policy.iam_policy_form.arn
+    role = aws_iam_role.iam_role_form.arn
     handler = "lambda_function.lambda_handler"
     source_code_hash = data.archive_file.lambda.output_base64sha256
     runtime = "python3.12"

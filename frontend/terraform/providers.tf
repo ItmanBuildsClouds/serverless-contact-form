ef1475c60037d4a4151e2f0f1d7f09b7faf9f973
ItmanBuildsClouds <itmanpiotr.js@gmail.com>
2025-09-02 01:14:48 +0200

    feat: Add initial Terraform provider setup for frontend

---
+++ frontend/terraform/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+     random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+}
+provider "aws" {
+  region = var.aws_region
+}

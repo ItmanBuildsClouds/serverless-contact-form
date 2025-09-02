3c97e6ab97159f8f6b8589300f302eb9db1427d0
ItmanBuildsClouds <itmanpiotr.js@gmail.com>
2025-09-02 03:06:23 +0200

    Add apiURL and contactForm.reset()

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const subjectInput = document.getElementById('subjectInput');
     const messageTextarea = document.getElementById('messageTextarea');
 
+    const apiUrl = 'https://ulhhqf6ej4.execute-api.eu-central-1.amazonaws.com/prod/contact'
+
     contactForm.addEventListener('submit', function (event) {
 
         event.preventDefault();
@@ -18,9 +20,12 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log("Form sent! Data collected:");
         console.log(formData);
 
+        fetch(apiUrl, {
+            method: 'POST',
+            body: JSON.stringify(formData)
+        });
         alert("Form sent! Data collected:");
-
+        contactForm.reset();
 
     });
-
 });

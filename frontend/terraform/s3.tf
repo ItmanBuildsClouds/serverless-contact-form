d0c899d9d7f552440f50bae2b487de13a49af38a
ItmanBuildsClouds <itmanpiotr.js@gmail.com>
2025-09-04 01:25:29 +0200

    refactor: Make s3 bucket private

--- frontend/terraform/s3.tf
+++ frontend/terraform/s3.tf
@@ -18,35 +18,7 @@ resource "aws_s3_bucket_public_access_block" "s3_form_public_access" {
     block_public_acls = true
     ignore_public_acls = true
 
-    restrict_public_buckets = false
-    block_public_policy = false
+    restrict_public_buckets = true
+    block_public_policy = true
 }
-
-data "aws_iam_policy_document" "s3_form_bucket_policy_document" {
-  statement {
-    actions = ["s3:GetObject"]
-    principals {
-      type = "AWS"
-      identifiers = ["*"]
-    }
-    resources = ["${aws_s3_bucket.s3_form_bucket.arn}/*"]
-  }
-}
-
-
-resource "aws_s3_bucket_policy" "s3_form_policy" {
-  bucket = aws_s3_bucket.s3_form_bucket.id
-  policy = data.aws_iam_policy_document.s3_form_bucket_policy_document.json
-  
-  depends_on = [ aws_s3_bucket_public_access_block.s3_form_public_access ]
-}
-
-resource "aws_s3_bucket_website_configuration" "example" {
-  bucket = aws_s3_bucket.s3_form_bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-}
-
 

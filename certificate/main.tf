c371f9939403a0e1fbdb678f0603dcbd40cdcb12
ItmanBuildsClouds <itmanpiotr.js@gmail.com>
2025-09-03 14:17:34 +0200

    feat: Add data.tf with backend for remote state

---
+++ certificate/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+##for route 53
+provider "aws" {
+  region = "eu-central-1"
+}
+##for certificate
+provider "aws" {
+  region = var.aws_region
+  alias = "us-east-1"
+}
+data "aws_route53_zone" "domain_main" {
+    name = var.domain
+}
+
+resource "aws_acm_certificate" "certificate" {
+    provider = aws.us-east-1
+    domain_name = var.domain
+    subject_alternative_names = ["*.${var.domain}"]
+    validation_method = "DNS"
+
+    lifecycle {
+      create_before_destroy = true
+    }
+}
+
+resource "aws_route53_record" "route_53_record" {
+    for_each = {
+        for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+            name = dvo.resource_record_name
+            record = dvo.resource_record_value
+            type = dvo.resource_record_type
+        }
+    }
+    allow_overwrite = true
+    zone_id = data.aws_route53_zone.domain_main.zone_id
+    name = each.value.name
+    type = each.value.type
+    records = [each.value.record]
+    ttl = 60
+}
+
+resource "aws_acm_certificate_validation" "validation" {
+    provider = aws.us-east-1
+    certificate_arn = aws_acm_certificate.certificate.arn
+    validation_record_fqdns = [for record in aws_route53_record.route_53_record : record.fqdn]
+}
+
+output "certificate_arn" {
+    description = "ARN of validated ACM cert."
+    value = aws_acm_certificate_validation.validation.certificate_arn
+}
---
+++ certificate/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+}
+variable "project_name" {
+    type = string
+    default = "serverless-contact-form"
+}
+variable "domain" {
+    type = string
+    default = "cloud.itmanbuildsclouds.website"
+}
